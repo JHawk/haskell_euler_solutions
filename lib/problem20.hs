22980d3d0305a26b216475cbcd066f8952a8799e
Josh <josh.r.hawk@gmail.com>
2010-06-10 15:47:48 -0400

    added comments

--- lib/problem20.hs
+++ lib/problem20.hs
@@ -1,5 +1,9 @@
+-- TODO move to utils for reuse 
+
 split :: Integer -> [Integer]
 split = map (read.(:[])).show 
+
+-- Find the sum of digits in 100!
 
 problem20 :: Integer -> Integer 
 problem20 n = (sum.split.product) [1..n]
